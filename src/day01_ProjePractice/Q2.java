c5c254a2f39cb92394ea33c3bcdd16a45ad20358
OMEROZYIGIT <ozyigitomer9@gmail.com>
2023-08-07 00:25:37 +0300

    Q2 class updated

--- src/day01_ProjePractice/Q2.java
+++ src/day01_ProjePractice/Q2.java
@@ -6,32 +6,51 @@ public class Q2 {
     /*
        2. SORU
     Kullanıcı tarafından girilen metni "Sezar Şifreleme Yöntemi" ne göre şifreleyiniz.
+
     Sezar Şifreleme: Sezar şifrelemesi, ilk kez Romalı lider Jül Sezar tarafından kullanılmış olan şifreleme tekniğidir.
     Bu şifreleme en basit ve en bilinen şifreleme tekniklerinden biridir.
+
     Düz metindeki her harfin, alfabeden belirli bir sayı ileride konumlu olan bir harfle değiştirildiği bir şifreleme türüdür.
-    //hello ----> ıfmmp --->1 birim kaydırılmış hali
+
+    // hello ----> ıfmmp --->1 birim kaydırılmış hali
      */
 
     public static void main(String[] args) {
+
         System.out.print("Şifrelenecek metni giriniz: ");
         Scanner scan = new Scanner(System.in);
         String metin = scan.nextLine().toUpperCase();
+
         System.out.print("Kaydırma miktarını giriniz: ");
         int kaydirma = scan.nextInt();
+
         sifreleme(metin, kaydirma);
+
     }
 
     public static void sifreleme(String metin, int kaydirma) {
-        String sifrelenmisMetin = "";// ZEYNEP--- 3
+
+        // boş bir String data türünde variable tanımlayalım
+        String sifrelenmisMetin = "";                                // ZEYNEP--- 3
+
         String alfabe = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        // 0 dan başlayıp metin isimli variable'ın uzunluğu kadar ilerle
         for (int i = 0; i < metin.length(); i++) {
+
+            // karakterleri incele metindeki
             char karater = metin.charAt(i);
+            // metindek ikarakter değişkeni alfabede kaçıncı index'e denk geliyor.
             int karakterIndex = alfabe.indexOf(karater);
+
+            // (26       +    3) % 26 =  3
+            // karakterIndexin ve kaydırma miktarını ekledikten sonra alfabe uzunluğunu geçmemesi için alfabe
+            // uzunluğuna modu alınarak alfabe değerleri içinde yeniIndex değeri elde edilmiş oldu
             int yeniIndex = (karakterIndex + kaydirma) % alfabe.length();
-            //(26       +    3) % 26 =  3
-            //karakterIndexin ve kaydırma miktarını ekledikten sonra alfabe uzunluğunu geçmemesi için alfabe
-            //uzunluğuna modu alınarak alfabe değerleri içinde yeniIndex değeri elde edilmiş oldu
+
+            // sifrelenmiş dite tanımladığımız variable' yeni index' e göre harfleri sıralı olarak tanımla
             sifrelenmisMetin += alfabe.charAt(yeniIndex);
+
         }
         System.out.println("Şifrelenmiş metin: " + sifrelenmisMetin);
     }
