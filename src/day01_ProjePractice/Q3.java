2b362864a36fb0dc366d786dc403df1660884b18
OMEROZYIGIT <ozyigitomer9@gmail.com>
2023-08-06 17:23:26 +0300

    25 Temmuz 2023

---
+++ src/day01_ProjePractice/Q3.java
@@ -0,0 +1,14 @@
+package day01_ProjePractice;
+
+public class Q3 {
+    /*
+    3. SORU
+        MOUNTAIN ARRAY: Verilen herhangi bir arrayin Mountanin Array olup olmadığını kontrol ediniz.
+        Mountain array ==> [0,2,5,3,1]
+        Bir array elemanları en büyük değerine kadar sürekli artan, en büyük değerden sonra sürekli azalan değer alıyorsa "Mountain Array" dir.
+        Mountain array değildir ==>[5,2,7,1,4]
+     */
+    public static void main(String[] args) {
+
+    }
+}
